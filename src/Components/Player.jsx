d79b586e818f7f3b8a2f8d1c37b1b0f4e1ab9854
fissid <fissid.ss@gmail.com>
2023-11-05 15:32:51 +0300

    changes

---
+++ src/Components/Player.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+export default function Player({ name, symbol }) {
+  // editing mode to re-render the page based on the situation
+  const [editing, editingChanger] = useState(false);
+  //   default nameTag is just for showing the name
+  let nameTag = <span className="player-name">{name}</span>;
+
+  const editHandler = () => {
+    editingChanger(true);
+  };
+  //   if situation is in editing mode change the nameTag to an input
+  if (editing) {
+    nameTag = <input type="text" />;
+  }
+  return (
+    <li>
+      <span className="player">
+        {nameTag}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editHandler}>{editing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
