bb068d38ac9035560bf9ecc1cc2c5fb133be9cff
fissid <fissid.ss@gmail.com>
2023-11-05 19:14:42 +0300

    changes

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,19 +1,28 @@
+import React, { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function selectSquareHandler(rowIndex, colIndex) {
+    setGameBoard((prevState) => {
+      const updatedboard = [...prevState.map((innerArray) => [...innerArray])];
+      updatedboard[rowIndex][colIndex] = "X";
+      return updatedboard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => {
+      {gameBoard.map((row, rowIndex) => {
         return (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => {
                 return (
                   <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => selectSquareHandler(rowIndex, colIndex)}>{playerSymbol}</button>
                   </li>
                 );
               })}
