4380b5258cd3561946534f0faeb52218c41c7943
fissid <fissid.ss@gmail.com>
2023-11-05 18:00:24 +0300

    changes

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 export default function Player({ name, symbol }) {
   // editing mode to re-render the page based on the situation
   const [editing, editingChanger] = useState(false);
+  const [playerName, playerNameChnager] = useState(name);
   //   default nameTag is just for showing the name
-  let nameTag = <span className="player-name">{name}</span>;
+  let nameTag = <span className="player-name">{playerName}</span>;
 
   //   state is changing w.r.t prev state so must pass a function
   const editHandler = () => {
-    editingChanger((prevState) => {
-      return !prevState;
-    });
+    editingChanger((prevState) => !prevState);
   };
-  const nameInputHandler = () => {};
+
+  const nameInputHandler = (e) => {
+    playerNameChnager(e.target.value);
+  };
 
   //   if situation is in editing mode change the nameTag to an input
   if (editing) {
-    nameTag = <input type="text" required value={name} onChange={nameInputHandler} />;
+    nameTag = <input type="text" required value={playerName} onChange={nameInputHandler} />;
   }
 
   return (
